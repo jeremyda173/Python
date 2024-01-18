defa855ea7a746f03a523c567591f1c5be34da1d
jeremyda173 <jh4017034@gmail.com>
2024-01-18 15:13:54 -0400

    feat: create list.py

---
+++ Tips/List.py
@@ -0,0 +1,14 @@
+# Creando una lista
+
+list = list(range(1, 21))
+print("Lista de numeros del uno al veinte: ", list)
+
+demo_list = [1, "hello", 1.34, True, [1, 2, 3]]
+colors = ['red', 'green', 'blue', 'yellow']
+
+print(len(demo_list))
+# Salida: 5
+print(colors[-1])
+# Salida: yellow
+print('green' in colors)
+
