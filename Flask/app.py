616de90d22d35f8aba857d421c0b35df9a4bf871
jeremyda173 <jh4017034@gmail.com>
2024-02-22 15:20:50 -0400

    feat: upload Flask Python code

--- Flask/app.py
+++ Flask/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, Woooorld!'
+    name = "Yarisa"
+    return render_template('index.html', name=name)
 
 @app.route('/hello/<string:name>')
 def hello(name):
