a52c0ca0568f11472ff03167cce3ad5a3a78280d
jeremyda173 <jh4017034@gmail.com>
2024-02-29 14:22:53 -0400

    feat: add new Flask application

--- Flask_3/Blog.py
+++ Flask_3/Blog.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
-from flask_sqlchemy import SQLAlchemy
+# from flask_sqlchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config["SQLLCHEMY_DATABASE_URI"] = ""
+app.config["SQLLCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 app.config["SQLLCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
