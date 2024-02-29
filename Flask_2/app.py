c8a147208ac1c2165d45e8517adf7cfa70768a52
jeremyda173 <jh4017034@gmail.com>
2024-02-29 09:20:38 -0400

    feat: add new Flask files

--- Flask_2/app.py
+++ Flask_2/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/add")
+def add():
+    return render_template("add.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
