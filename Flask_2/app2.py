e10fc0a88aca237a57834d9e782bba03201f1a94
jeremyda173 <jh4017034@gmail.com>
2024-02-29 12:02:34 -0400

    feat: add new Flask files

--- Flask_2/app2.py
+++ Flask_2/app2.py
@@ -8,14 +8,15 @@ tareas = []
 def home():
     return render_template("index.html", tareas=tareas)
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == 'GET':
-        return render_template("add.html", methods=["GET", "POST"])
+        return render_template("add.html")
     else:
         tarea = request.form['task']
         tareas.append(tarea)
         return redirect("/")
-    
+
+
 if __name__ == '__main__':
     app.run(debug=True)
