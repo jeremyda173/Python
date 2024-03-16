a06051e550b416f312521ceee77f02032e5a89fd
jeremyda173 <jh4017034@gmail.com>
2024-03-16 16:42:09 -0400

    chore: modify comments

--- Convertidores-numericos/convertidor.py
+++ Convertidores-numericos/convertidor.py
@@ -28,7 +28,7 @@ def convertir():
 root = tk.Tk()
 root.title("Convertidor de Sistemas Numéricos")
 
-# Etiqueta de entrada
+#Etiqueta de entrada
 tk.Label(root, text="Número:").grid(row=0, column=0, padx=5, pady=5)
 
 # Campo de entrada
