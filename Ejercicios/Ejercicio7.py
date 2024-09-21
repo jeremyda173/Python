d02c59130fbacdad5cc3dbe787f3a972be4608eb
jeremyda173 <jh4017034@gmail.com>
2024-09-21 13:21:11 -0400

    feat: add new Code

---
+++ Ejercicios/Ejercicio7.py
@@ -0,0 +1,5 @@
+num1 = int(input("Introduce el primer numero: "))
+num2 = int(input("Introduce el primer numero: "))
+res = num1 + num2
+if res == 10: 
+    print("El resultado de la suma es:", res, ". Por eso se activa la condicion de -->", "Hello world")
